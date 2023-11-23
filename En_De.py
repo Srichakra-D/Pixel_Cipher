25528c3c4208f12a246702bd9acdfb685d4f58ef
Srichakra <srichakradevarakonda5@gmail.com>
2023-11-23 22:03:19 +0530

    Added Comments

--- En_De.py
+++ En_De.py
@@ -1,25 +1,37 @@
 from cryptography.fernet import Fernet
 
-def Encrypt(key,plain_text):
+def Encrypt(key, plain_text):
+    
+    # Encode the plain text to bytes
+    byte_text = plain_text.encode('utf-8')
 
-    bite_text=plain_text.encode('utf-8')
-
+    # Create a Fernet cipher suite using the provided key
     cipher_suite = Fernet(key)
-    cipher_text = cipher_suite.encrypt(bite_text)
+    
+    # Encrypt the byte text
+    cipher_text = cipher_suite.encrypt(byte_text)
 
+    # Convert the encrypted byte text to a UTF-8 encoded string
     cipher_text = cipher_text.decode('utf-8')
-    cipher_text = cipher_text+'%'
-        
+
+    # Add a delimiter '%' at the end of the cipher text for later extraction
+    cipher_text = cipher_text + '%'
+
     return cipher_text
 
-def Decrypt(key,cipher_text):
 
-    cipher_text=cipher_text.encode('utf-8')
+def Decrypt(key, cipher_text):
+    
+    # Encode the cipher text to bytes
+    cipher_text = cipher_text.encode('utf-8')
 
+    # Create a Fernet cipher suite using the provided key
     cipher_suite = Fernet(key)
+
+    # Decrypt the cipher text
     decrypted_text = cipher_suite.decrypt(cipher_text)
 
-    decrypted_text=decrypted_text.decode('utf-8')
+    # Convert the decrypted byte text to a UTF-8 encoded string
+    decrypted_text = decrypted_text.decode('utf-8')
 
     return decrypted_text
-
--- main.py
+++ main.py
@@ -1,88 +1,90 @@
 import argparse
 import cv2
 import os
-import En_De as ED
+import En_De as ED  
 
-d={}
-c={}
+# Dictionary mapping characters to their ASCII values and vice versa
+d = {}
+c = {}
 for i in range(255):
-    d[chr(i)]=i
+    d[chr(i)] = i
     c[i] = chr(i)
 
-key=b'XFSn0xe3I9_IXrF3CFLiKLsYiHtexklllbu0KvwVoy4='
+# Encryption key
+key = b'XFSn0xe3I9_IXrF3CFLiKLsYiHtexklllbu0KvwVoy4='
 
-
+# Command-line argument parser
 main_parser = argparse.ArgumentParser(description='Embed or extract a message from a cover file')
 
-main_parser.add_argument('--embed','-em',action='store_true',help='enables embedding')
-main_parser.add_argument('--extract','-ex',action='store_true',help='enables extracting')
-main_parser.add_argument('--messagefile','-mf',help='Input Message file')
-main_parser.add_argument('--coverfile','-cf',help='Input Cover file')
-main_parser.add_argument('--stegofile','-sf',help='output file')
-main_parser.add_argument('--open','-of',help='input file for extracting')
-main_parser.add_argument('--extractfile','-xf',help='extracted file')
+# Arguments for embedding
+main_parser.add_argument('--embed', '-em', action='store_true', help='enables embedding')
+main_parser.add_argument('--extract', '-ex', action='store_true', help='enables extracting')
+main_parser.add_argument('--messagefile', '-mf', help='Input Message file')
+main_parser.add_argument('--coverfile', '-cf', help='Input Cover file')
+main_parser.add_argument('--stegofile', '-sf', help='output file')
+main_parser.add_argument('--open', '-of', help='input file for extracting')
+main_parser.add_argument('--extractfile', '-xf', help='extracted file')
 
 args = main_parser.parse_args()
 
-
 if args.embed:
-
-    message_file=args.messagefile
-    cover_file=args.coverfile
-    stego_file=args.stegofile
+    # Embedding process
+    message_file = args.messagefile
+    cover_file = args.coverfile
+    stego_file = args.stegofile
 
     with open(f'{message_file}', 'r') as file:
         message = file.read()
 
-    encoded_message=ED.Encrypt(key,message)
-    
+    #Encoding the given message
+    encoded_message = ED.Encrypt(key, message)
 
     img = cv2.imread(f"{cover_file}")
 
+    m = 0
+    n = 0
+    z = 0
 
-    m=0
-    n=0
-    z=0
-
+    # Embedding the encoded message into the image
     for i in range(len(encoded_message)):
-        img[n,m,z] = d[encoded_message[i]] 
-        n=n+1
-        m=m+1
-        z=(z+1)%3
+        img[n, m, z] = d[encoded_message[i]]
+        n = n + 1
+        m = m + 1
+        z = (z + 1) % 3
 
-    cv2.imwrite(f"{stego_file}",img)
+    
+    cv2.imwrite(f"{stego_file}", img)
 
-    os.system(f"start {stego_file}")
-
-
+    os.system(f"open {stego_file}")
 
 elif args.extract:
-
+    # Extraction process
     open_file = args.open
     extract_file = args.extractfile
 
     img_ = cv2.imread(f"{open_file}")
 
-    cipher_message=""
+    cipher_message = ""
 
-    n=0
-    m=0
-    z=0
+    n = 0
+    m = 0
+    z = 0
 
-    while(True):
-        if c[img_[n,m,z]] == "%":
+    # Extracting the cipher message from the image
+    while True:
+        if c[img_[n, m, z]] == "%":
             break
-        
         else:
-            cipher_message = cipher_message + c[img_[n,m,z]]
+            cipher_message = cipher_message + c[img_[n, m, z]]
 
-            n=n+1
-            m=m+1
-            z=(z+1) %3
+            n = n + 1
+            m = m + 1
+            z = (z + 1) % 3
 
-    decoded_message=ED.Decrypt(key,cipher_message)
-    
+    #Decoding the message from cipher text
+    decoded_message = ED.Decrypt(key, cipher_message)
+
     with open(f'{extract_file}', 'w') as file:
         file.write(decoded_message)
-        
-    os.system(f"start {extract_file}")
+
+    os.system(f"open {extract_file}")
